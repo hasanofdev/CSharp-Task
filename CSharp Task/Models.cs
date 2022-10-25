d0ed5b2e0b9407a7dd350ad978fbee59c26f6062
Hese_fz25 <Hese_fz25@STHQ0126-12>
2022-10-25 16:30:23 +0400

    Add project files.

---
+++ CSharp Task/Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Task
+{
+    public class DocumentProgram
+    {
+        public void OpenDocument() { Console.WriteLine("Document Opened"); }
+        public virtual void EditDocument() => Console.WriteLine("Can Edit in Pro and Expert versions");
+        public virtual void SaveDocument() => Console.WriteLine("Can Save in Pro and Expert versions");
+    }
+
+    public class ProDocumentProgram:DocumentProgram
+    {
+        public sealed override void EditDocument() => Console.WriteLine("Document Edited");
+        public override void SaveDocument() => Console.WriteLine("Document Saved in doc format, for pdf format buy Expert packet");
+    }
+
+    public class ExpertDocument: ProDocumentProgram
+    {
+        public override void SaveDocument() => Console.WriteLine("Document Saved in pdf format");
+    }
+}
---
+++ CSharp Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp_Task;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Key: ");
+        string? key = Console.ReadLine();
+
+        DocumentProgram? document = null;
+
+        switch (key.ToLower())
+        {
+            case "basic":
+                document = new DocumentProgram();
+                break;
+            case "pro":
+                document = new ProDocumentProgram();
+                break;
+            case "expert":
+                document = new ExpertDocument();
+                break;
+            default:
+                throw new InvalidDataException();
+        }
+
+
+        document.OpenDocument();
+        document.EditDocument();
+        document.SaveDocument();
+    }
+} 
